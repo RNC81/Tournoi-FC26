138e1c973599bf8517bcd326320e22c4e30a33d1
RNC81 <rayaannathoo@gmail.com>
2025-11-18 12:53:49 +0100

    etape5_V2

--- backend/server.py
+++ backend/server.py
@@ -703,4 +703,4 @@ async def shutdown_db_client():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True))
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True)
